1d05e6d46d3c6688bc36e6b39010b7ed08975104
alvin0319 <alvin7810@naver.com>
2021-10-09 15:55:24 +0900

    Removed dead codes

--- src/main/kotlin/com/github/wow/pvc/EventListenerImpl.kt
+++ src/main/kotlin/com/github/wow/pvc/EventListenerImpl.kt
@@ -16,8 +16,6 @@ class EventListenerImpl(private val bot: Bot) : EventListener {
 
     private val logger = LoggerFactory.getLogger(javaClass)
 
-    private val preventDeleteMap: MutableMap<Long, Long> = mutableMapOf()
-
     override fun onEvent(event: GenericEvent) {
         when (event) {
             is MessageReceivedEvent -> onMessageReceived(event)
@@ -63,10 +61,6 @@ class EventListenerImpl(private val bot: Bot) : EventListener {
 
         if (event.channelLeft != null) {
             val channel = event.channelLeft!!
-            if (preventDeleteMap.contains(event.member.idLong) && preventDeleteMap.getValue(event.member.idLong) == channel.idLong) {
-                preventDeleteMap.remove(event.member.idLong)
-                return
-            }
             if (guild.voiceChannels.any { it.id == channel.idLong }) {
                 logger.debug(channel.members.size.toString())
                 if (channel.members.size <= 0) {
@@ -76,10 +70,6 @@ class EventListenerImpl(private val bot: Bot) : EventListener {
         }
         if (event.channelJoined != null) {
             val channel = event.channelJoined!!
-            if (preventDeleteMap.contains(event.member.idLong) && preventDeleteMap.getValue(event.member.idLong) == channel.idLong) {
-                preventDeleteMap.remove(event.member.idLong)
-                return
-            }
             if (guild.creationChannels.any { it.id == channel.idLong }) {
                 bot.createChannels(event.guild, event.member, channel.parent)
             }
@@ -91,9 +81,6 @@ class EventListenerImpl(private val bot: Bot) : EventListener {
         val guild = bot.getGuildDataOrSave(event.guild)
 
         if (guild.creationChannels.any { it.id == channel.idLong }) {
-            channel.members.forEach {
-                preventDeleteMap[it.idLong] = channel.idLong
-            }
             bot.deleteCreatedChannels(channel)
         }
     }
